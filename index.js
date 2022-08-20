d45b710e2c5883a6afc69a5bb223017a3ed1d949
olamideare <63550820+olamideare@users.noreply.github.com>
2022-08-20 08:53:44 +0100

    delete functionality, DOM Local storage,

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-// chrome://extensions/
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+// 1. Store the delete button in a deleteBtn variable
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+let deleteBtn =  document.getElementById("delete-btn")
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+deleteBtn.addEventListener('dblclick', function clickTwice() {
+    console.log("Double Click Works")
+    
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
 })
 
